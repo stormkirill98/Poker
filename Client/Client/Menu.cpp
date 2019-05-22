98009bab37ef08ce20a478bf7c11cc441b39d365
stormkirill98 <stormkirill98@gmail.com>
2019-05-22 22:47:29 +0300

    create menu dialog

---
+++ Client/Client/Menu.cpp
@@ -0,0 +1,59 @@
+﻿// Menu.cpp: файл реализации
+//
+
+#include "stdafx.h"
+#include "Client.h"
+#include "Menu.h"
+#include "afxdialogex.h"
+
+
+// Диалоговое окно Menu
+
+IMPLEMENT_DYNAMIC(Menu, CDialogEx)
+
+Menu::Menu(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MENU, pParent)
+{
+
+}
+
+Menu::~Menu()
+{
+}
+
+void Menu::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_PORT, m_Port);
+	DDX_Control(pDX, IDC_SERVER, m_Server);
+	DDX_Control(pDX, IDC_USER_NAME, m_UserName);
+}
+
+
+BEGIN_MESSAGE_MAP(Menu, CDialogEx)
+	ON_BN_CLICKED(IDC_CONNECT, &Menu::OnClickedConnect)
+	ON_BN_CLICKED(IDC_DISCONNECT, &Menu::OnClickedDisconnect)
+	ON_BN_CLICKED(IDCANCEL, &Menu::OnBnClickedCancel)
+END_MESSAGE_MAP()
+
+
+// Обработчики сообщений Menu
+
+
+void Menu::OnClickedConnect()
+{
+	// TODO: добавьте свой код обработчика уведомлений
+}
+
+
+void Menu::OnClickedDisconnect()
+{
+	// TODO: добавьте свой код обработчика уведомлений
+}
+
+
+void Menu::OnBnClickedCancel()
+{
+	// TODO: добавьте свой код обработчика уведомлений
+	CDialogEx::OnCancel();
+}
---
+++ Client/Client/Menu.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+
+// Диалоговое окно Menu
+
+class Menu : public CDialogEx
+{
+	DECLARE_DYNAMIC(Menu)
+
+public:
+	Menu(CWnd* pParent = nullptr);   // стандартный конструктор
+	virtual ~Menu();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MENU };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnClickedConnect();
+	afx_msg void OnClickedDisconnect();
+	afx_msg void OnBnClickedCancel();
+
+	CEdit m_Port;
+	CEdit m_Server;
+	CEdit m_UserName;
+};

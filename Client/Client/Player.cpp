697f400014dbcfded5e274c4a64eb372c6e12d0b
stormkirill98 <stormkirill98@gmail.com>
2019-05-27 23:10:36 +0300

    draw player info's

--- Client/Client/Player.cpp
+++ Client/Client/Player.cpp
@@ -9,19 +9,11 @@ Player::Player()
 Player::Player(CString name)
 	: m_Name(name)
 {
-	m_Cards[0] = Card(-1, -1);
-	m_Cards[1] = Card(-1, -1);
 }
 
 
 Player::~Player()
 {
-}
-
-void Player::SetCards(Card card1, Card card2)
-{
-	m_Cards[0] = card1;
-	m_Cards[1] = card2;
 }
 
 void Player::SetBet(int bet)
@@ -37,11 +29,6 @@ void Player::SetBank(int bank)
 CString Player::GetName()
 {
 	return m_Name;
-}
-
-Card * Player::GetCards()
-{
-	return m_Cards;
 }
 
 int Player::GetBet()
--- Client/Client/Player.h
+++ Client/Client/Player.h
@@ -10,18 +10,15 @@ public:
 
 private:
 	CString m_Name;
-	Card m_Cards[2];
 	int m_Bet = 0;
 	int m_Bank = 0;
 
 public:
-	void SetCards(Card card1, Card card2);
 	void SetBet(int bet);
 	void SetBank(int bank);
 
 public:
 	CString GetName();
-	Card* GetCards();
 	int GetBet();
 	int GetBank();
 };
